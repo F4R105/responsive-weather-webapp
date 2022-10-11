074a769bcdf99488c55267beb93b45287c5e9123
F4R105 <kajubufaraji@gmail.com>
2022-10-11 14:31:47 +0300

    Temperature data fetch according to location and positioned a degree symbol

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # responsive-weather-webapp
+
+### Features to be added
+- LocalStorage to store user chosen theme
+- Display time of the last API call
+- Button to refresh to current temperature ie. API Recall
+- Add a user permission request page
+- Display user's location ie. reverse geocoding
--- js/script.js
+++ js/script.js
@@ -1,36 +1,22 @@
+const getTemperature = () => {
 
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(async loc=>{
 
-// const latitude = 
-// const longitude = 
-// const timezone = 
+            const latitude = loc.coords.latitude
+            const longitude = loc.coords.longitude
+            const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`
+            const data = await fetch(url)
+            const weather = await data.json()
+        
+            const hour = new Date().getHours()
+            const temperature = Math.floor(weather.hourly.temperature_2m[hour])
+        
+            document.querySelector('#temperature').innerHTML = `${temperature} <sup>o</sup>`
 
+        }, error=>console.log(error))
+    }
 
-// fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=temperature_2m_max`)
-// .then(data=>data.json())
-// .then(data=>console.log(data))
-
-
-// if(navigator.geolocation){
-//     navigator.geolocation.getCurrentPosition(async loc=>{
-//         const latitude = await loc.coords.latitude
-//         const longitude = await loc.coords.longitude
-
-//         console.log('Latitude', latitude)
-//         console.log('Longitude', longitude)
-
-//         // await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max&timezone=Africa/Dar_es_Salaam`)
-//         await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&timezone=Africa/Dar_es_Salaam`)
-//         .then(data=>data.json())
-//         .then(data=>{
-//             console.log(data)
-//             console.log(data.daily.temperature_2m_max)
-//             console.log(data.daily.time)
-//             console.log(data.daily_units.temperature_2m_max)
-//         })
-
-//     })
-// }
-
-if(navigator.geolocation){
-    console.log(navigator.geolocation)
 }
+
+getTemperature()
